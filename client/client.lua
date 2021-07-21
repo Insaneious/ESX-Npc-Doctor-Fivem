adf8977e17211a22e7d50c71c5a1047e70dd6f2d
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-07-21 12:19:47 +0100

    Velike promjene izmjene, optimizacija :)

--- client/client.lua
+++ client/client.lua
@@ -1,77 +1,47 @@
 ESX = nil
-local PlayerData = {}
-
-CreateThread(function() while ESX == nil do 
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) Wait(0) end
-    while ESX.GetPlayerData().job == nil do Wait(10) end
-    ESX.PlayerData = ESX.GetPlayerData()
-end)
-
-RegisterNetEvent('esx:playerLoaded')
-AddEventHandler('esx:playerLoaded', function(xPlayer)
-    ESX.PlayerData = xPlayer
-end)
-
-RegisterNetEvent('esx:setJob')
-AddEventHandler('esx:setJob', function(job)
-    ESX.PlayerData.job = job
-end)
-
+local canEms -- definisimo local.
 CreateThread(function() while ESX == nil do
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) Wait(0) end
+    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) Wait(200) end
 end)
-
-CreateThread(function() while true do
-    local sleep = 5000
-    local _source = source
-    local ped = PlayerPedId()
-    local pedCoords = GetEntityCoords(ped)
-
+CreateThread(function()
+    Wait(2000)
+    while true do
+    local spavaj = 5000
+    local igrac = PlayerPedId()
+    local kordinateigraca = GetEntityCoords(igrac)
     for i = 1, #Config.Doktori, 1 do
-        local doctor = Config.Doktori[i]
-        local userDst = GetDistanceBetweenCoords(pedCoords, doctor.x, doctor.y, doctor.z, true)
-
-        if userDst <= 15 then
-            sleep = 2
-            if userDst <= 5 then
-                DrawText3D(doctor.x, doctor.y, doctor.z, 'Pritisnite [E] Da se ozivite $'.. Config.cjenaDoktora)
-                DrawMarker(20, doctor.x, doctor.y, doctor.z-0.100, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 2.0, 2.0, 1.0, 255, 195, 18, 100, false, true, 2, false, false, false, false)
-                if userDst <= 1.5 then
-                if IsControlJustPressed(0, 38) then
+        local doktor = Config.Doktori[i]
+        local udaljenost = #(kordinateigraca - vec3(doktor.x, doktor.y, doktor.z))
+            if udaljenost <= 5 then
+                spavaj = 7
+                DrawText3D(doktor.x, doktor.y, doktor.z, 'Pritisnite [E] Da se ozivite $'.. Config.cjenaDoktora)
+                DrawMarker(20, doktor.x, doktor.y, doktor.z-0.100, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 2.0, 2.0, 1.0, 255, 195, 18, 100, false, true, 2, false, false, false, false)
+                if udaljenost <= 1.5 then
+                    if IsControlJustReleased(0, 38) then
                       ESX.TriggerServerCallback('premium:gettajMedikale', function(cb)
                             canEms = cb
                         end, i)
-                        while canEms == nil do
+                            while not canEms do
                                 Wait(0)
                             end
                             if Config.DoktoriLimit then
-                                if canEms == true then
-                                    
+                                if canEms then
                                     ESX.TriggerServerCallback('premium-doctor:kontrola', function(hasEnoughMoney)
                                         if hasEnoughMoney then
-                                            local formattedCoords = {
-                                                x = ESX.Math.Round(pedCoords.x, 1),
-                                                y = ESX.Math.Round(pedCoords.y, 1),
-                                                z = ESX.Math.Round(pedCoords.z, 1)
-                                            }
+                                            local formattedCoords = {x = ESX.Math.Round(kordinateigraca.x, 1),y = ESX.Math.Round(kordinateigraca.y, 1),z = ESX.Math.Round(kordinateigraca.z, 1)}
                                             TriggerEvent('esx_ambulancejob:revive', formattedCoords)
                                             TriggerServerEvent('premium-doctor:novac')
                                         else
                                             exports['mythic_notify']:DoHudText('error', 'Nemate dovoljno novca!')
                                         end
                                     end)
-
                                 elseif canEms == 'no_ems' then
-                                    exports['mythic_notify']:DoHudText('error', 'U gradu ima dovoljno Doktora da vas ljece !')                          
-                                end 
+                                    exports['mythic_notify']:DoHudText('error', 'U gradu ima dovoljno Doktora da vas ljece !')
+                                end
                             else
                                 ESX.TriggerServerCallback('premium-doctor:kontrola', function(hasEnoughMoney)
                                     if hasEnoughMoney then
-                                        local formattedCoords = {
-                                            x = ESX.Math.Round(pedCoords.x, 1),
-                                            y = ESX.Math.Round(pedCoords.y, 1),
-                                            z = ESX.Math.Round(pedCoords.z, 1)
-                                        }
+                                        local formattedCoords = {x = ESX.Math.Round(kordinateigraca.x, 1),y = ESX.Math.Round(kordinateigraca.y, 1),z = ESX.Math.Round(kordinateigraca.z, 1)}
                                         TriggerEvent('esx_ambulancejob:revive', formattedCoords)
                                         TriggerServerEvent('premium-doctor:novac')
                                     else
@@ -83,8 +53,7 @@ CreateThread(function() while true do
                     end
                 end
             end
-        end
-        Wait(sleep)
+        Wait(spavaj)
     end
 end)
 
@@ -104,13 +73,11 @@ function DrawText3D(x,y,z,text,size)
 end
 
 CreateThread(function()
-    RequestModel(GetHashKey("s_m_m_doctor_01"))
-    while not HasModelLoaded(GetHashKey("s_m_m_doctor_01")) do
-        Wait(1)
-    end
+    RequestModel(`s_m_m_doctor_01`)
+    while not HasModelLoaded(`s_m_m_doctor_01`) do Wait(10) end
 	if Config.UkljuciPedove then
         for _, doctor in pairs(Config.Doktori) do
-			local npc = CreatePed(4, 0xD47303AC, doctor.x, doctor.y, doctor.z-1.0, doctor.heading, false, true)
+			local npc = CreatePed(4, `s_m_m_doctor_01`, doctor.x, doctor.y, doctor.z-1.0, doctor.heading, false, true)
 			SetEntityHeading(npc, doctor.heading)
 			FreezeEntityPosition(npc, true)
 			SetEntityInvincible(npc, true)
@@ -128,7 +95,7 @@ CreateThread(function()
             SetBlipScale  (blip, 1.0)
             SetBlipColour (blip, 2)
             SetBlipAsShortRange(blip, true)
-            BeginTextCommandSetBlipName("STRING")
+            BeginTextCommandSetBlipName('STRING')
             AddTextComponentString('Bolnicar')
             EndTextCommandSetBlipName(blip)
         end
